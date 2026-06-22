b8a5915d312d49a64bb477b9e04026d62d3aff89
Nootan <Nootan@STPLAL50.swajyot.local>
2026-06-22 11:33:00 +0530

    up

--- src/components/calendar/CalendarView.jsx
+++ src/components/calendar/CalendarView.jsx
@@ -1551,7 +1551,7 @@ useEffect(() => {
         <div className="h-full p-4">
           <Calendar
             localizer={localizer}
-            events={events.filter(e => !e.isOriginal)}
+            events={events.filter(e => !e.isOriginal)}  // ← This filters out ALL events
             startAccessor="start"
             endAccessor="end"
             view={view}
@@ -1919,7 +1919,7 @@ useEffect(() => {
               ) : (
                 <Calendar
                   localizer={localizer}
-                  events={events.filter(e => !e.isOriginal)}
+                  events={events.filter(e => !e.isOriginal)}  // ← This filters out ALL events
                   startAccessor="start"
                   endAccessor="end"
                   view={view}
